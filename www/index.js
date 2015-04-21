c813b0febf30df993ff55c293835702f6261f62e
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-04-21 12:27:11 +0200

    test pluigiojn 25

--- www/index.js
+++ www/index.js
@@ -64,7 +64,7 @@ function testIDB(){
 }
 
 function testID(){
-    DeleteDatabase("idarticle_people");
+    //DeleteDatabase("idarticle_people");
     var openRequest = indexedDB.open("idarticle_people",1);
  
     openRequest.onupgradeneeded = function(e) {
